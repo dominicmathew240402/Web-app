3dbc691a90b39833841e9047085fc515918d4020
dominicmathew240402 <kingdominic240@gmail.com>
2023-09-03 17:00:00 +0800

    commit

--- deepsort.py
+++ deepsort.py
@@ -1,16 +1,17 @@
 # Python In-built packages
+import os
 from pathlib import Path
 import PIL
+import cv2
+import numpy as np
 
 # External packages
 import streamlit as st
-import numpy as np
-from deep_sort_realtime.deepsort_tracker import Tracker
 
 # Local Modules
 import settings
 import helper
-import result
+from deep_sort_realtime.deepsort_tracker import DeepSort, Detection
 
 # Setting page layout
 st.set_page_config(
@@ -82,28 +83,68 @@ if source_radio == settings.IMAGE:
                      use_column_width=True)
         else:
             if st.sidebar.button('Detect Objects'):
-                res = model.predict(uploaded_image,
-                                    conf=confidence
-                                    )
+                res = model.predict(uploaded_image, conf=confidence)
                 boxes = res[0].boxes
+
+                # Tracking objects with DeepSORT
+                frame = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
+                
+                # Convert boxes to the required format (raw_detections)
+                raw_detections = [(bbox, 1.0) for bbox in boxes]  # Use a constant confidence of 1.0
+
+                # Update tracks using the raw detections
+                trackers = DeepSort.update_tracks(frame, raw_detections)
+
+                # Create the "train" folder if it doesn't exist
+                train_folder = 'train'
+                os.makedirs(train_folder, exist_ok=True)
+
+                # Extract unique IDs for tracking
+                unique_ids = set()
+                for track in trackers:
+                    unique_ids.add(track.track_id)
+
+                # Save up to 10 snapshots with unique IDs
+                saved_snapshots = 0
+                for track in trackers:
+                    if saved_snapshots >= 10:
+                        break
+                    if track.track_id in unique_ids:
+                        unique_ids.remove(track.track_id)
+                        x, y, w, h = track.to_tlwh()
+                        crop = frame[int(y):int(y + h), int(x):int(x + w)]
+
+                        # Save the snapshot with a unique ID
+                        snapshot_path = f"train/snapshot_{track.track_id}.jpg"
+                        cv2.imwrite(snapshot_path, crop)
+                        saved_snapshots += 1
 
                 count = 0
 
                 res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image',
-                         use_column_width=True)
+                st.image(res_plotted, caption='Detected Image', use_column_width=True)
+
                 try:
                     with st.expander("Detection Results"):
                         for box in boxes:
-                            count+=1
+                            count += 1
 
-                            box_confidence = box.conf
+                            # Accessing object-level confidence is not available in the provided structure
+                            # box_confidence = box.conf
+
                             st.write(box.data)
-                            st.write(f"Confidence : {box_confidence}")
+                            # st.write(f"Confidence : {box_confidence}")
 
                         count1 = count - 1
                         st.write(f"Total Object Detected: {count1}")
-                
+
+                    with st.expander("Tracking Results"):
+                        for track in trackers:
+                            x, y, w, h = track.to_tlwh()
+                            track_id = track.track_id
+                            st.write(f"Track ID: {track_id}")
+                            st.write(f"Bounding Box: ({x:.2f}, {y:.2f}, {x + w:.2f}, {y + h:.2f})")
+
                 except Exception as ex:
                     # st.write(ex)
                     st.write("No image is uploaded yet!")
